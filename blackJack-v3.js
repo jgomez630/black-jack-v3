f91db871deec124f0a920e4cfc876be7bc83bc33
Jeff <jgomez630@aol.com>
2018-06-09 13:38:17 -0700

    created arrays for playerCards and deck

---
+++ blackJack-v3.js
@@ -0,0 +1,14 @@
+let deck = [
+	'ace of spades',
+	'two of spades',
+	'three of spades'	
+];
+
+let playerCards = [ deck[0], deck[2] ];
+    
+
+console.log('Welcome to Black Jack');
+
+console.log('Your hand is: ');
+console.log(' ' + playerCards[0] );
+console.log(' ' + playerCards[1] );
