312fde22d3e08b10e806c741f04b986a5179ef34
Jeff <jgomez630@aol.com>
2018-06-10 00:40:03 -0700

    wrapped createDeck code into a function

--- blackJack-v3.js
+++ blackJack-v3.js
@@ -1,10 +1,24 @@
-let deck = [
-	'ace of spades',
-	'two of spades',
-	'three of spades'	
-];
+let suits = [ 'Hearts', 'Clubs', 'Diamonds', 'Spades' ];
+let values = [ 'Ace', 'King', 'Queen', 'Jack', 'Ten', 'Nine', 
+'Eight', 'Seven', 'Six', 'Five', 'Four', 'Three', 'Two' ];
 
-let playerCards = [ deck[0], deck[2] ];
+function createDeck(){
+	let deck = [];
+	for ( var suitIdx = 0; suitIdx < suits.length; suitIdx++ ) {
+		for ( var valueIdx = 0; valueIdx < values.length; valueIdx++ ){
+			deck.push( values[valueIdx] + ' of ' + suits[suitIdx] );
+		}
+ 	}
+ 	return deck;	
+}
+
+function getNextCard(){
+	return deck.shift();
+}
+
+let deck = createDeck();
+
+let playerCards = [ getNextCard(),getNextCard() ];
     
 
 console.log('Welcome to Black Jack');
